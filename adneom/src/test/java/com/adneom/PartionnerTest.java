fc38a21ebdc3771734e193d3308bd2c9300a67a6
sanaURCA <sana.arbi@gmail.com>
2019-09-25 16:33:22 +0200

    Update PartionnerTest.java

--- adneom/src/test/java/com/adneom/PartionnerTest.java
+++ adneom/src/test/java/com/adneom/PartionnerTest.java
@@ -5,55 +5,44 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
-public class PartionnerTest {
+@RunWith(Parameterized.class)
+public class PartionnerTest<T> {
+
+	@Parameters
+	public static Collection<Object[]> data(){
+		return Arrays.asList(new Object[][] {
+			{Arrays.asList(1, 2, 3, 4, 5),1, Arrays.asList(Arrays.asList(1), Arrays.asList(2), Arrays.asList(3), Arrays.asList(4), Arrays.asList(5))},
+			{Arrays.asList(1, 2, 3, 4, 5),2,Arrays.asList(Arrays.asList(1,2),Arrays.asList(3,4), Arrays.asList(5))},
+			{Arrays.asList(1, 2, 3, 4, 5),3,Arrays.asList(Arrays.asList(1,2,3),Arrays.asList(4,5))},
+			{Arrays.asList(1, 2, 3, 4, 5),8,Arrays.asList(Arrays.asList(1,2,3,4,5))}	
+		});
+	}
+	
+	@Parameterized.Parameter(0)
+	public List<T> list;
+	@Parameterized.Parameter(1)
+	public int partitionSize;
+	@Parameterized.Parameter(2)
+	public List<List<T>> newList;
+	
+
+	@Test
+	public void should_return_list_when_partitionSize() {
+		assertEquals(newList, Partionner.partition(list, partitionSize));
+	}
 	
 	@Test(expected = IllegalArgumentException.class)
 	public void throw_exception_when_list_is_null() {
 		List<Integer> list = null;
 		Partionner.partition(list, 2);
-	}
-
-	@Test
-	public void should_return_1345814_when_list_1345814_and_partition_size_8() {
-		List<Integer> list = Arrays.asList(1,3,4,5,8,1,4);
-		List<List<Integer>> newList = Partionner.partition(list, 8);
-		assertTrue(newList.size() == 1);
-		assertEquals(newList.get(0), Arrays.asList(1,3,4,5,8,1,4));
-	}
-	
-	@Test
-	public void should_return_12_34_5_when_list_12345_and_partition_size_2() {
-		List<Integer> list = Arrays.asList(1,2,3,4,5);
-		List<List<Integer>> newList = Partionner.partition(list, 2);
-		assertTrue(newList.size() == 3);
-		assertEquals(newList.get(0), Arrays.asList(1,2));
-		assertEquals(newList.get(1), Arrays.asList(3,4));
-		assertEquals(newList.get(2), Arrays.asList(5));
-	}
-	
-	@Test
-	public void should_return_123_45_when_list_12345_and_partition_size_3() {
-		List<Integer> list = Arrays.asList(1,2,3,4,5);
-		List<List<Integer>> newList = Partionner.partition(list, 3);
-		assertTrue(newList.size() == 2);
-		assertEquals(newList.get(0), Arrays.asList(1,2,3));
-		assertEquals(newList.get(1), Arrays.asList(4,5));
-	}
-	
-	@Test
-	public void should_return_1_2_3_4_5_when_list_12345_and_partition_size_1(){
-		List<Integer> list = Arrays.asList(1,2,3,4,5);
-		List<List<Integer>> newList = Partionner.partition(list, 1);
-		assertTrue(newList.size() == 5);
-		assertEquals(newList.get(0), Arrays.asList(1));
-		assertEquals(newList.get(1), Arrays.asList(2));
-		assertEquals(newList.get(2), Arrays.asList(3));
-		assertEquals(newList.get(3), Arrays.asList(4));
-		assertEquals(newList.get(4), Arrays.asList(5));
 	}
 	
 	@Test
